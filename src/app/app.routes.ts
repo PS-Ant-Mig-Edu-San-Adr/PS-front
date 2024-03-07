758256bbbed5d4adc76d4ac90b6d9f38a8497405
EduardoCole <eduortza@gmail.com>
2024-03-07 11:24:25 +0000

    comienzo diseño horarios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { OrganizationsComponent } from './organizations/organizations.component';
+import { SchedulesComponent } from './schedules/schedules.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'organizations',  component: OrganizationsComponent}
+  { path: 'organizations',  component: OrganizationsComponent},
+  { path: 'schedules',  component: SchedulesComponent}
 ];
---
+++ src/app/schedules/schedules.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { SharedPopupsService } from '../generalServices/sharedPopups.service';
+import { HeaderComponent } from '../header/header.component';
+import { LoginComponent } from '../login/login.component';
+import { FooterComponent } from '../footer/footer.component';
+import { LoginService } from '../login/login.component.service';
+import { RegisterService } from '../register/register.component.service';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from '../register/register.component';
+
+@Component({
+  selector: 'app-schedules',
+  standalone: true,
+  imports: [CommonModule, HeaderComponent, LoginComponent, FooterComponent, RegisterComponent],
+  providers: [SharedPopupsService],
+  templateUrl: './schedules.component.html',
+  styleUrl: './schedules.component.css'
+})
+export class SchedulesComponent {
+  constructor(public sharedService: SharedPopupsService, public loginService: LoginService, public registerService: RegisterService) {}
+
+  ngOnInit() {
+    this.sharedService.loginService.isOpen$.subscribe((success: boolean) => {
+      this.sharedService.toggleWrapperContainerStyles(success);
+    });
+
+    this.sharedService.registerService.isOpen$.subscribe((success: boolean) => {
+      this.sharedService.toggleWrapperContainerStyles(success);
+    });
+  }
+}
