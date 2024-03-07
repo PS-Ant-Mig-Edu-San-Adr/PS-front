ad01c5080e216223dfecca0eb32668a0ad0fe02e
Trepuco03 <106118289+Trepuco03@users.noreply.github.com>
2024-03-07 21:21:28 +0000

    Hecho gestionar grupos

---
+++ src/app/admin-create-organization/admin-create-organization.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedPopupsService } from '../generalServices/sharedPopups.service';
+import { HeaderComponent } from '../header/header.component';
+import { LoginComponent } from '../login/login.component';
+import { FooterComponent } from '../footer/footer.component';
+import { LoginService } from '../login/login.component.service';
+import { RegisterService } from '../register/register.component.service';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from '../register/register.component';
+import { AdminButtonsComponent } from '../admin-buttons/admin-buttons.component';
+
+@Component({
+  selector: 'app-admin-create-organization',
+  standalone: true,
+  imports: [CommonModule, HeaderComponent, LoginComponent, FooterComponent, RegisterComponent, AdminButtonsComponent],
+  providers: [SharedPopupsService],
+  templateUrl: './admin-create-organization.component.html',
+  styleUrl: './admin-create-organization.component.css'
+})
+export class AdminCreateOrganizationComponent implements OnInit{
+  constructor(public sharedService: SharedPopupsService, public loginService: LoginService, public registerService: RegisterService) {}
+
+  ngOnInit() {
+    this.sharedService.loginService.isOpen$.subscribe((success: boolean) => {
+      this.sharedService.toggleWrapperContainerStyles(success);
+    });
+
+    this.sharedService.registerService.isOpen$.subscribe((success: boolean) => {
+      this.sharedService.toggleWrapperContainerStyles(success);
+    });
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,15 @@ import { OrganizationsComponent } from './organizations/organizations.component'
 import { SchedulesComponent } from './schedules/schedules.component';
 import { AdminAddEventComponent } from './admin-add-event/admin-add-event.component';
 import { AdminOrganizationsComponent } from './admin-organizations/admin-organizations.component';
-
+import {AdminCreateOrganizationComponent} from  "./admin-create-organization/admin-create-organization.component";
+import { AdminGroupsComponent } from "./admin-groups/admin-groups.component";
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'organizations',  component: OrganizationsComponent},
   { path: 'schedules',  component: SchedulesComponent},
   { path: 'admin', component: AdminAddEventComponent},
-  { path: 'adminOrganizations', component: AdminOrganizationsComponent}
+  { path: 'adminOrganizations', component: AdminOrganizationsComponent},
+  { path: 'adminCreateOrganization', component: AdminCreateOrganizationComponent },
+  { path: 'adminGroups', component: AdminGroupsComponent}
 ];
