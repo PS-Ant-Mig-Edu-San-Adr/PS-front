d3079b838fc0e69fd18b3d46e936bf24e8a36761
Trepuco03 <106118289+Trepuco03@users.noreply.github.com>
2024-03-10 13:33:29 +0000

    Navegabilidad  popUpManageMembers

--- src/app/admin-create-organization/admin-create-organization.component.ts
+++ src/app/admin-create-organization/admin-create-organization.component.ts
@@ -8,17 +8,19 @@ import { RegisterService } from '../register/register.component.service';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from '../register/register.component';
 import { AdminButtonsComponent } from '../admin-buttons/admin-buttons.component';
+import { ManageMembersService } from '../manage-members-pop-up/manage-members-pop-up.component.service'
+import { ManageMembersPopUpComponent } from '../manage-members-pop-up/manage-members-pop-up.component'
 
 @Component({
   selector: 'app-admin-create-organization',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, LoginComponent, FooterComponent, RegisterComponent, AdminButtonsComponent],
+  imports: [CommonModule, HeaderComponent, LoginComponent, FooterComponent, RegisterComponent, AdminButtonsComponent, ManageMembersPopUpComponent],
   providers: [SharedPopupsService],
   templateUrl: './admin-create-organization.component.html',
   styleUrl: './admin-create-organization.component.css'
 })
 export class AdminCreateOrganizationComponent implements OnInit{
-  constructor(public sharedService: SharedPopupsService, public loginService: LoginService, public registerService: RegisterService) {}
+  constructor(public manageMembersService: ManageMembersService, public sharedService: SharedPopupsService, public loginService: LoginService, public registerService: RegisterService) {}
 
   ngOnInit() {
     this.sharedService.loginService.isOpen$.subscribe((success: boolean) => {
@@ -28,5 +30,11 @@ export class AdminCreateOrganizationComponent implements OnInit{
     this.sharedService.registerService.isOpen$.subscribe((success: boolean) => {
       this.sharedService.toggleWrapperContainerStyles(success);
     });
+    this.sharedService.manageMembersService.isOpen$.subscribe((success: boolean) => {
+      this.sharedService.toggleWrapperContainerStyles(success);
+    });
+  }
+  manageMembers() {
+    this.manageMembersService.openManageMembersPopup();
   }
 }
--- src/app/admin-groups/admin-groups.component.ts
+++ src/app/admin-groups/admin-groups.component.ts
@@ -8,17 +8,20 @@ import { RegisterService } from '../register/register.component.service';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from '../register/register.component';
 import { AdminButtonsComponent } from '../admin-buttons/admin-buttons.component';
+import { ManageMembersService } from '../manage-members-pop-up/manage-members-pop-up.component.service'
+import { ManageMembersPopUpComponent } from '../manage-members-pop-up/manage-members-pop-up.component'
+
 
 @Component({
   selector: 'app-admin-groups',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, LoginComponent, FooterComponent, RegisterComponent, AdminButtonsComponent],
+  imports: [CommonModule, HeaderComponent, LoginComponent, FooterComponent, RegisterComponent, AdminButtonsComponent, ManageMembersPopUpComponent],
   providers: [SharedPopupsService],
   templateUrl: './admin-groups.component.html',
   styleUrl: './admin-groups.component.css'
 })
 export class AdminGroupsComponent  implements OnInit {
-  constructor(public sharedService: SharedPopupsService, public loginService: LoginService, public registerService: RegisterService) {}
+  constructor(public manageMembersService: ManageMembersService, public sharedService: SharedPopupsService, public loginService: LoginService, public registerService: RegisterService) {}
 
   ngOnInit() {
     this.sharedService.loginService.isOpen$.subscribe((success: boolean) => {
@@ -28,5 +31,11 @@ export class AdminGroupsComponent  implements OnInit {
     this.sharedService.registerService.isOpen$.subscribe((success: boolean) => {
       this.sharedService.toggleWrapperContainerStyles(success);
     });
+    this.sharedService.manageMembersService.isOpen$.subscribe((success: boolean) => {
+      this.sharedService.toggleWrapperContainerStyles(success);
+    });
+  }
+  manageMembers() {
+    this.manageMembersService.openManageMembersPopup();
   }
 }
