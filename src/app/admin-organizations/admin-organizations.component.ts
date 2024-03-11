66bdf4de1122b130439b9a326d6d9691462a2ee6
Santiramoli <sanolydani@gmail.com>
2024-03-11 10:03:29 +0000

    Admin organizations component updated.

--- src/app/admin-organizations/admin-organizations.component.ts
+++ src/app/admin-organizations/admin-organizations.component.ts
@@ -8,17 +8,19 @@ import { RegisterService } from '../register/register.component.service';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from '../register/register.component';
 import { AdminButtonsComponent } from '../admin-buttons/admin-buttons.component';
+import { ManageActivitiesPopUpComponent } from '../manage-activities-pop-up/manage-activities-pop-up.component';
+import { ManageActivitiesPopUpService } from '../manage-activities-pop-up/manage-activities-pop-up.service';
 
 @Component({
   selector: 'app-admin-organizations',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, LoginComponent, FooterComponent, RegisterComponent, AdminButtonsComponent],
+  imports: [CommonModule, HeaderComponent, LoginComponent, FooterComponent, RegisterComponent, AdminButtonsComponent, ManageActivitiesPopUpComponent],
   providers: [SharedPopupsService],
   templateUrl: './admin-organizations.component.html',
   styleUrl: './admin-organizations.component.css'
 })
 export class AdminOrganizationsComponent implements  OnInit{
-  constructor(public sharedService: SharedPopupsService, public loginService: LoginService, public registerService: RegisterService) {}
+  constructor(public addActivitiesPopUP: ManageActivitiesPopUpService, public sharedService: SharedPopupsService, public loginService: LoginService, public registerService: RegisterService) {}
 
   ngOnInit() {
     this.sharedService.loginService.isOpen$.subscribe((success: boolean) => {
@@ -28,6 +30,11 @@ export class AdminOrganizationsComponent implements  OnInit{
     this.sharedService.registerService.isOpen$.subscribe((success: boolean) => {
       this.sharedService.toggleWrapperContainerStyles(success);
     });
+    this.sharedService.addActivitiesService.isOpen$.subscribe((success: boolean) => {
+      this.sharedService.toggleWrapperContainerStyles(success);
+    });
   }
-
+  addActivities(){
+    this.addActivitiesPopUP.openAddActivityPopup();
+  }
 }
