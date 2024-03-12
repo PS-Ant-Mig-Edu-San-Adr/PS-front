2b3eff792b37cdfed268434eb359555dc31f0d8d
Trepuco03 <106118289+Trepuco03@users.noreply.github.com>
2024-03-12 11:17:59 +0000

    Inputs perfil, no se puede editar si no das click

--- src/app/perfil-info/perfil-info.component.ts
+++ src/app/perfil-info/perfil-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit,  EventEmitter, Output } from '@angular/core';
 import { SharedPopupsService } from '../generalServices/sharedPopups.service';
 import { HeaderComponent } from '../header/header.component';
 import { LoginComponent } from '../login/login.component';
@@ -8,6 +8,9 @@ import { RegisterService } from '../register/register.component.service';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from '../register/register.component';
 import { PerfilButtonsComponent } from '../perfil-buttons/perfil-buttons.component'
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
 @Component({
   selector: 'app-perfil-info',
   standalone: true,
@@ -20,6 +23,8 @@ import { PerfilButtonsComponent } from '../perfil-buttons/perfil-buttons.compone
 export class PerfilInfoComponent implements OnInit {
   constructor(public sharedService: SharedPopupsService, public loginService: LoginService, public registerService: RegisterService) {}
 
+  active: number = 0;
+  
   ngOnInit() {
     this.sharedService.loginService.isOpen$.subscribe((success: boolean) => {
       this.sharedService.toggleWrapperContainerStyles(success);
@@ -28,5 +33,33 @@ export class PerfilInfoComponent implements OnInit {
     this.sharedService.registerService.isOpen$.subscribe((success: boolean) => {
       this.sharedService.toggleWrapperContainerStyles(success);
     });
+  }
+
+  
+  @ViewChild('inputNoActive', { static: false }) inputNoActive!: ElementRef;
+
+
+  toggleEditMode(inputElement: HTMLElement) {
+    if (inputElement) {
+      const inputId = inputElement.getAttribute('id');
+  
+
+      const newId = inputId === 'noactive' ? 'active' : 'noactive';
+      inputElement.setAttribute('id', newId);
+  
+      if (newId === 'active') {
+        if (inputElement.tagName === 'INPUT') {
+          (inputElement as HTMLInputElement).removeAttribute('readonly'); 
+        } else if (inputElement.tagName === 'SELECT') {
+          (inputElement as HTMLSelectElement).removeAttribute('disabled');
+        }
+      } else {
+        if (inputElement.tagName === 'INPUT') {
+          (inputElement as HTMLInputElement).setAttribute('readonly', 'true'); 
+        } else if (inputElement.tagName === 'SELECT') {
+          (inputElement as HTMLSelectElement).setAttribute('disabled', 'true'); 
+        }
+      }
+    }  
   }
 }
