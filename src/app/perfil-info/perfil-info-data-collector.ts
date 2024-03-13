0ecda68a0c5b0fb06dfaf822872064fc4b3c58ab
EduardoOrtegaZerpa <eduortza@gmail.com>
2024-03-13 00:55:30 +0000

    conexión de modificar datos perfil con backend

--- src/app/perfil-info/perfil-info-data-collector.ts
+++ src/app/perfil-info/perfil-info-data-collector.ts
@@ -1,13 +1,14 @@
 import { User } from "../interfaces/interface";
 
 export class PerfilInfoDataCollector {
-  static collectUserData(user: User): any {
+  static collectUserData(user: User, profilePict: File | undefined): any {
 
     // Get references to HTML elements
     const inputUsername = document.getElementById('input-username') as HTMLInputElement;
     const inputPassword = document.getElementById('input-password') as HTMLInputElement;
     const inputEmail = document.getElementById('input-email') as HTMLInputElement;
-    const inputZona = document.getElementById('input-zona') as HTMLInputElement;
+    const inputZone = document.getElementById('input-zone') as HTMLInputElement;
+
 
 
 
@@ -16,15 +17,17 @@ export class PerfilInfoDataCollector {
       username: inputUsername.value,
       password: inputPassword.value,
       email: inputEmail.value,
-      zona: inputZona.value
+      zona: inputZone.value,
+      profilePict: profilePict,
     };
 
-    // Validate form fields
-    if (!this.validateFormFields(eventData)) {
-      return {result: null, details: "Please complete all required fields."};
-    }
-
-    if (!this.validateUserFields(user, eventData)) {
+    // Validate the data
+    if (eventData.profilePict) {
+      const imgValidation = this.verifySizeandImg(eventData.profilePict);
+      if (imgValidation !== true) {
+        return imgValidation;
+      }
+    }else if (this.validateUserFields(user, eventData)) {
       return {result: null, details: "Please do not enter the same data as before."};
     }
 
@@ -33,12 +36,49 @@ export class PerfilInfoDataCollector {
       return {result: null, details: "Please enter a valid email."};
     }
 
+    // Validate password
+    if (eventData.password && ! this.verifyPass(eventData.password)) {
+      return {result: null, details: "Please enter a valid password."};
+    }
+
+    // Validate image
+    
+
     return {result: eventData, details: "Succesfully collected data."};
   }
-  private static validateFormFields(eventData: any): boolean {
-    // Check if any required field is missing
-    return (eventData.username || eventData.password || eventData.email || eventData.zona);
 
+  private static verifySizeandImg(file: File): any {
+    if (!file) {
+      return {result: null, details: "Please enter a file."};
+    }
+    if (file.size > 10000000) {
+      return {result: null, details: "Please enter a file smaller than 10MB."};
+    }
+    const imgRegex = /^image\/(jpeg|jpg|png)$/; // Expresión regular para tipos de imagen comunes
+    if (!imgRegex.test(file.type)) {
+        return { result: null, details: "Please enter a valid image file."};
+    }
+    return true;
+  }
+
+  private static verifyPass(password: string): boolean {
+    if (password.length < 8) {
+      return false;
+    }
+    
+    if (!/[a-z]/.test(password)) {
+      return false;
+    }
+    
+    if (!/[A-Z]/.test(password)) {
+      return false;
+    }
+    
+    if (!/\d/.test(password)) {
+      return false;
+    }
+    
+    return true;
   }
 
   private static validateEmail(email: string): boolean {
@@ -49,9 +89,8 @@ export class PerfilInfoDataCollector {
 
   static validateUserFields(user: User, eventData: any): boolean {
     return (
-      (user.username && user.username === eventData.username) ||
-      (user.passwordHash && user.passwordHash === eventData.passwordHash) ||
-      (user.email && user.email === eventData.email) ||
+      (user.username && user.username === eventData.username) &&
+      (user.email && user.email === eventData.email) &&
       (user.timeZone && user.timeZone === eventData.zona)
     );
   }
