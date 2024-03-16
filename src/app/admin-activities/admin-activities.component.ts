417a58173a4e6c3afd44bd65c9f191e3847bdd63
Santiramoli <sanolydani@gmail.com>
2024-03-16 11:35:13 +0000

    Cascade Dropdown List functionality implemented for the interface to manage activities.

--- src/app/admin-activities/admin-activities.component.ts
+++ src/app/admin-activities/admin-activities.component.ts
@@ -11,10 +11,9 @@ import {ManageMembersPopUpComponent} from '../manage-members-pop-up/manage-membe
 import {GroupAddPopUpComponent} from '../group-add-pop-up/group-add-pop-up.component';
 import {GroupAddPopUpService} from '../group-add-pop-up/group-add-pop-up.service';
 import {AuthService} from "../generalServices/auth-service/auth.service";
-import { HttpClient } from '@angular/common/http';
-import { Organization } from '../interfaces/interface';
+import { Activity, Organization } from '../interfaces/interface';
 import { OrganizationService } from '../generalServices/organization.service';
-import { response } from 'express';
+import { SessionStorageService } from 'angular-web-storage';
 
 @Component({
   selector: 'app-admin-activities',
@@ -27,16 +26,24 @@ import { response } from 'express';
 export class AdminActivitiesComponent implements  OnInit {
   constructor(public addGroup: GroupAddPopUpService, protected authService: AuthService,
               public manageMembersService: ManageMembersService, public sharedService: SharedPopupsService,
-              private organizationService: OrganizationService) {}
+              private organizationService: OrganizationService,
+              private sessionStorageService: SessionStorageService) {}
   active: number = 3;
   organizations: Organization[] = [];
+  activities: any;
   
+  @ViewChild('inputName', {static: false}) inputName!: ElementRef<HTMLInputElement>;
+  @ViewChild('inputDescription', {static: false}) inputDescription!: ElementRef<HTMLTextAreaElement>;
+  @ViewChild('inputGroupName', {static: false}) inputGroupName!: ElementRef<HTMLInputElement>;
 
   ngOnInit() {
 
-    this.organizationService.getOrganizations().subscribe(data => {
-        console.log(data);
-      });
+    this.organizationService.getOrganizationsByUsername(this.sessionStorageService.get("username")).subscribe(data => {
+        this.organizations = data.organizations;
+        this.activities = this.organizations[0].activities;
+        this.inputName.nativeElement.value = this.activities[0].name;
+        this.inputDescription.nativeElement.value = this.activities[0].description;
+    });
 
     this.sharedService.authService.isLoginOpen$() .subscribe((success: boolean) => {
       this.sharedService.toggleWrapperContainerStyles(success);
@@ -58,6 +65,32 @@ export class AdminActivitiesComponent implements  OnInit {
 
   manageMembers() {
     this.manageMembersService.openManageMembersPopup();
+  }
+
+  loadActivity(selectElement: HTMLSelectElement){
+    const selectedIndex_org = selectElement.selectedIndex;
+    const selectedOrg = this.organizations[selectedIndex_org];
+    this.activities = selectedOrg.activities;
+    this.inputName.nativeElement.value = this.activities[0].name;
+    this.inputDescription.nativeElement.value = this.activities[0].description;
+  }
+
+  loadActivityInfo(selectElement: HTMLSelectElement){
+    const selectedActName = selectElement.value;
+    const selectedAct = this.activities.find((activity: Activity) => activity.name === selectedActName);
+    if (selectedAct) {
+        this.inputName.nativeElement.value = selectedAct.name;
+        this.inputDescription.nativeElement.value = selectedAct.description;
+    }
+  }
+
+  checkEmpty(){
+    if(!this.inputName.nativeElement.value){
+      alert("Por favor, inserte un título para la actividad.");
+    }
+    if(!this.inputGroupName.nativeElement.value){
+      alert("Por favor, inserte un nombre para el grupo.");
+    }
   }
 
   @ViewChild('inputNoActive', { static: false }) inputNoActive!: ElementRef;
